3342d5d735bb903ff6172abb8a97643d9e6d9121
Leonchuly <5628113@qq.com>
2018-12-11 00:20:03 +0800

    加入了blog

--- designwork/designwork/urls.py
+++ designwork/designwork/urls.py
@@ -18,9 +18,11 @@ from django.urls import path
 from .import views
 from django.conf.urls.static import static
 from django.conf import settings
+import blog.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.main),
+    path('blog/', blog.views.blog_page),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
