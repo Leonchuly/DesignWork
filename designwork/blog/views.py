14fb8dc3d8e6289d8d2d8dc8ad254d6aa8b06d29
Leonchuly <5628113@qq.com>
2018-12-12 01:03:50 +0800

    增加了blog详细页面

--- designwork/blog/views.py
+++ designwork/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
@@ -6,3 +6,6 @@ def blog_page(request):
     blogs = Blog.objects
     return render(request, 'blog.html' ,{'blog':blogs})
 
+def blog_contant(request,blog_id):
+    blogc = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog_contant.html', {'blogc':blogc})
